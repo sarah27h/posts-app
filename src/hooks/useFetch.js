7360dcc7a3107096574a56cad418a55a458ad4fd
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2021-02-15 00:57:28 +0200

    Fix: use clean fuction + AbortController to abort fetch for unmouted component

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -9,8 +9,13 @@ export default function useFetch(url) {
   //   used to run our JSON fake server
   //npx json-server --watch data/db.json --port 8000
   useEffect(() => {
+    // 2
+    // create an instance
+    const abortcont = new AbortController();
     setTimeout(() => {
-      fetch(url)
+      // 3
+      // associate instance with fetch
+      fetch(url, { signal: abortcont.signal })
         .then((res) => {
           if (!res.ok) {
             throw Error('Could not fetch resource data');
@@ -22,11 +27,25 @@ export default function useFetch(url) {
           setPending(false);
           setErrorMessage(null);
         })
+        // 5
+        // abort fetch will cause error and it get catched
+        //
         .catch((err) => {
-          setPending(false);
-          setErrorMessage(err.message);
+          if (err.name === 'AbortError') {
+            console.log('fetch aborted');
+          } else {
+            // auto catches network / connection error
+            setPending(false);
+            setErrorMessage(err.message);
+          }
         });
     }, 1000);
+
+    // 1
+    // clean up function to abort fetch
+    // 4
+    // abort any fetch associate with abortCont
+    return () => abortcont.abort();
   }, [url]);
 
   return { data, pending, errorMessage };
