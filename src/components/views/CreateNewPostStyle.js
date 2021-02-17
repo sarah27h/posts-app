5c439b6a8f5731a1d59b9cf0948383c54828a9f2
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2021-02-17 02:00:11 +0200

    Style: create <CreateNewPost />

---
+++ src/components/views/CreateNewPostStyle.js
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles((theme) => ({
+  formControl: {
+    width: '100%',
+  },
+  formFieldsMargin: {
+    marginTop: '16px',
+    marginBottom: '8px',
+  },
+  formField: {},
+}));
